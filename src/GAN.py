bb04e52d712873bb27879edceb38d425cd60d336
vonalan <kingdom_lee@outlook.com>
2017-11-29 20:51:00 +0800

    m+a+d

--- src/GAN.py
+++ src/GAN.py
@@ -3,17 +3,6 @@
 import numpy as np
 import tensorflow as tf
 
-def pool(x, kernel_size=[1, 2, 2, 1], strides=[1,2,2,1], name='pool'):
-    # TODO: how to implemente tf.nn.max_pool_with_argmax with cpu?
-    with tf.variable_scope(name) as scope:
-        with tf.device('/gpu:0'):
-            return tf.nn.max_pool_with_argmax(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                                              padding='SAME', name=scope)
-
-def unpool_2x2(x, pool_argmax, output_shape):
-    pass
-
-
 def conv2d(x, kernel_sizes, bias_sizes, stride=[1,1,1,1],
            activation=None,
            use_batch_normalization=False,
@@ -45,17 +34,56 @@ def conv2d_transpose(x, kernel_sizes, bias_sizes, stride=[1,1,1,1],
         out = tf.nn.relu(out, name=name)
         return [kernel, biases], out
 
-def build_discriminator():
-    x = tf.placeholder(tf.float32, [64, 320, 320, 3])
-    _, y = conv2d(x, [5,5,3,64], [64], [1,2,2,1], name='conv2d')
-    _, z = conv2d_transpose(y, [5,5,3,64], [3], [1,2,2,1], [64,320,320,3], name='deconv2d')
-    return y, z
+def build_convnet_graph(inputs):
+    _, conv1 = conv2d(inputs, [5, 5, 3, 64], [64], [1, 2, 2, 1], name='conv1')
+    _, conv2 = conv2d(conv1, [5, 5, 64, 128], [128], [1, 2, 2, 1], name='conv2')
+    _, conv3 = conv2d(conv2, [5, 5, 128, 256], [256], [1, 2, 2, 1], name='conv3')
+    _, conv4 = conv2d(conv3, [5, 5, 256, 512], [512], [1, 2, 2, 1], name='conv4')
+    _, conv5 = conv2d(conv4, [5, 5, 512, 1024], [1024], [1, 2, 2, 1], name='conv5')
+    _, deconv5 = conv2d_transpose(conv5, [5, 5, 512, 1024], [512], [1, 2, 2, 1], [10, 5, 5, 512], name='deconv5')
+    _, deconv4 = conv2d_transpose(deconv5, [5, 5, 256, 512], [256], [1, 2, 2, 1], [10, 10, 10, 512], name='deconv4')
+    _, deconv3 = conv2d_transpose(deconv4, [5, 5, 128, 256], [128], [1, 2, 2, 1], [10, 20, 20, 512], name='deconv3')
+    _, deconv2 = conv2d_transpose(deconv3, [5, 5, 64, 128], [64], [1, 2, 2, 1], [10, 40, 40, 512], name='deconv2')
+    _, deconv1 = conv2d_transpose(deconv2, [5, 5, 3, 64], [3], [1, 2, 2, 1], [10, 40, 40, 512], name='deconv1')
+    return conv5, deconv1
+
+def load_images():
+    import os
+    from PIL import Image
+    image_dir = '../images'
+
+    RGBs = []
+    for i, image in enumerate(os.listdir(image_dir)):
+        image_path = os.path.join(image_dir, image)
+        RGBs.append(np.array(Image.open(image_path)).astype(np.int))
+    return np.array(RGBs)
+
+def main():
+    import itertools
+    import random
+    raw_rgbs = tf.placeholder(tf.float32, [10, 96, 96, 3], name='raw_rgbs')
+    _, pred_rgbs = build_convnet_graph(raw_rgbs)
+    c_diff = tf.sqrt(tf.square(pred_rgbs - raw_rgbs) + 1e-12)/255.0
+    loss = tf.reduce_sum(c_diff)
+    tf.summary.image('raw_RGBs', raw_rgbs, max_outputs=2)
+    tf.summary.image('pred_RGBs', pred_rgbs, max_outputs=2)
+    tf.summary.scalar('loss', loss)
+    optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
+    merger = tf.summary.merge_all()
+    summary_writer = tf.summary.FileWriter('../temp/logs/train', tf.get_default_graph())
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    for i in itertools.count():
+        images = load_images()
+        print(images.shape)
+        for i in itertools.count():
+            random.shuffle(images)
+            _, summary, rgbs = sess.run([optimizer, merger, pred_rgbs], feed_dict={raw_rgbs: images[:10,:,:,:]})
+            summary_writer.add_summary(summary, i)
+            print('epoch: %d', i)
+            print(rgbs)
 
 if __name__ == '__main__':
-    y, z = build_discriminator()
-    z_shape = tf.shape(z)
-
-    sess = tf.Session()
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    print(sess.run(z_shape))
+    main()
